ba07be8147521a0e86d5e70950e07545cc9eb21e
Marc Merino <marcmerino24@gmail.com>
2021-03-15 15:23:49 +0100

    Heroku push

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const app = express()
 var cors = require('cors')
-const port = 3000
+const PORT = process.env.PORT || 5000
 var MongoClient = require('mongodb').MongoClient;
 const uri = "mongodb+srv://admin:DbLv98QyYq6hawu@cluster0.dzgdm.mongodb.net?retryWrites=true&w=majority";
 
-app.use(cors())
+app.use(cors());
 
 app.get('/users', (req, res) => {
   queryUsers(req, res);
@@ -20,5 +20,5 @@ async function queryUsers(req, res) {
 }
 
 app.listen(port, () => {
-  console.log(`API ready and listening at port 3000`);
+  console.log(`API ready and listening at port 5000`);
 })
