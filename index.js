fcce8c17d308939537fb5dda5bd021af542bfa9c
MarcMerino <marcmerino24@gmail.com>
2021-04-14 18:55:07 +0200

    Changed bodyparser options again

--- index.js
+++ index.js
@@ -15,8 +15,8 @@ var item = {};
 var secretkey = "";
 const fs = require("fs");
 
-app.use(express.bodyParser.urlencoded({limit: '50mb', extended: false }));
-app.use(express.bodyParser.json({ limit: '500mb' }));
+app.use(express.urlencoded({limit: '50mb', extended: false }));
+app.use(express.json({ limit: '500mb' }));
 app.use(cors());
 
 // Default:
